a746604255da3bbba4598a4d2d6efe2098f25648
Dariusz1989 <dariusz1989@gmail.com>
2021-07-05 09:42:09 +0100

    cleanup

--- lib/controller.cpp
+++
@@ -1,9 +0,0 @@
-/*!
- *@author Dariusz Makowski
- *@date 05/07/2021    
- *Part of "Project Icarus"
- *Created by Dariusz Makowski on 05/07/2021.
- *Copyright (c) 2021 Dariusz Makowski LTD All rights reserved.
-*/
-
-#include "controller.h"
